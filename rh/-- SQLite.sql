8567e3803d5e1fa538e9c9ea5d0b79371fa153d6
sophia.armond <sophis.armond14@gmail.com>
2022-07-11 20:34:00 -0300

    Versão inicial do projeto

---
+++ rh/-- SQLite.sql
@@ -0,0 +1,16 @@
+-- SQLite
+-- SQL (Sctructured Query Language)
+-- MDL (Manipulate Data Language)
+
+-- Seleciona todos os dados da tabela
+SELECT * FROM Rh_departamento;
+
+INSERT INTO Rh_departamento (id, nome)
+VALUES (6, 'RH');
+
+INSERT INTO Rh_departamento (id, nome)
+VALUES (7, 'Produção');
+
+-- ORM (OBJECT RELATIONAL MAPPING)
+
+
---
+++ rh/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from rh.models import Departamento, Funcionario
+
+# Register your models here.
+admin.site.register (Funcionario)
+admin.site.register (Departamento)
